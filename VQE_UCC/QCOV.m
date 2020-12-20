fbfed269cad95d9bed2284dbeea7855eaca06ca6
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2020-12-20 21:44:04 +0100

    Add files via upload

--- VQE_UCC/QCOV.m
+++ VQE_UCC/QCOV.m
@@ -6,6 +6,7 @@ function QSigma = QCOV(r)
     p = r(2:2:length(r));%transpose(r(2:2:length(r)));
     B = [x,p];
     C = [ones(l,1), 1i*ones(l,1); ones(l,1), -1i*ones(l,1)];
-    QSigma = 0.25*(B.*C)*V.*(conj(C).*conj(B))+0.5*ones(2*l,l); 
+    eta = 0.5*ones(2*l,2*l);
+    QSigma = 0.25*reshape(B(:).*C(:),4,2)*V*reshape(conj(C(:)).*conj(B(:)),2,4)+eta; 
 %    QSigma = 0.25*(B.*C)*V.*(conj(C).*conj(B))+0.5*ones(2*l,l); 
 end
